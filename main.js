544cae5ea404f7dcc2c6385a165f6076c0a63163
Muxtorbek <ravshanovmuxtorbek0115@gmail.com>
2021-10-04 12:25:33 +0500

    codlar yozildi

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 "use strict";
+
+const numberOfNews = +prompt("Siz qancha yangilik ko'rdingiz");
+
+const personalNewsDB = {
+  count: numberOfNews,
+  news: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Oxirgi ko'rgan yangiliklaringiza biri"),
+    b = prompt("Unga qancha baho bergan bo'lar edingiz?");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalNewsDB.news[a] = b;
+    console.log("tayyor");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+if (personalNewsDB.count < 10) {
+  console.log("Juda oz sonli yangiliklar o'qlibdi");
+} else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30) {
+  console.log("Siz klassik tamoshabinsiz");
+} else if (personalNewsDB.count >= 30) {
+  console.log("Siz yangliklar ishqibozi ekansiz");
+} else {
+  console.log("Xato yuz berdi");
+}
+
+console.log(personalNewsDB);
